34372678f58547e50ee771a995cf2bb0c8b3d0f3
John Alzate <157264782+Johnki1@users.noreply.github.com>
2025-08-29 11:24:08 -0500

    Update PingController.java

--- Backend/src/main/java/com12/facturacion/controllers/PingController.java
+++ Backend/src/main/java/com12/facturacion/controllers/PingController.java
@@ -1,1 +1,13 @@
+package com12.facturacion.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
